3b34b4ba90521c661424bde55a7f22746895ae86
anbguye <anthonybnguyen56@gmail.com>
2024-11-13 20:03:11 -0600

    added character sprite animations

--- game.go
+++ game.go
@@ -15,6 +15,12 @@ var (
 	playerSprite rl.Texture2D //player sprite
 	playerSrc    rl.Rectangle  //player source rectangle
 	playerDest   rl.Rectangle  //player destination rectangle
+	playerMoving bool //player moving
+	playerDir int //player direction
+	playerUp, playerDown, playerLeft, playerRight bool //player up, down, left, right
+	playerFrame int //player frame
+
+	frameCount int //frame count
 
 	playerSpeed float32 = 3 //player speed
 
@@ -25,22 +31,30 @@ var (
 )
 
 func drawScene() { //draw scene
-	rl.DrawTexture(grassSprite, 100, 50, rl.White)
-	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0 , rl.White)
+	rl.DrawTexture(grassSprite, 100, 50, rl.White) //draw grass sprite
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0 , rl.White) //draw player sprite
 }
 
 func input() { //get input
 	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp){ //check if W or up key is pressed
-		playerDest.Y -= playerSpeed //move player up
+		playerMoving = true //player moving
+		playerDir = 1 //player direction
+		playerUp = true //player up
 	}
 	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown){ //check if S or down key is pressed
-		playerDest.Y += playerSpeed //move player down
+		playerMoving = true //player moving
+		playerDir = 0 //player direction
+		playerDown = true //player down
 	}
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight){ //check if D or right key is pressed
-		playerDest.X += playerSpeed //move player right
+		playerMoving = true //player moving
+		playerDir = 3 //player direction
+		playerRight = true //player right
 	}
 	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft){ //check if A or left key is pressed
-		playerDest.X -= playerSpeed //move player left
+		playerMoving = true //player moving
+		playerDir = 2 //player direction
+		playerLeft = true //player left
 	}
 	if rl.IsKeyPressed(rl.KeyQ){ //check if Q key is pressed
 		musicPaused = !musicPaused //toggle music paused
@@ -49,6 +63,36 @@ func input() { //get input
 
 func update(){ //update game
 	running = !rl.WindowShouldClose() //check if window should close
+
+	playerSrc.X = 0 //reset player source rectangle x
+
+	if playerMoving { //check if player moving
+		if playerUp { //check if player up
+			playerDest.Y -= playerSpeed //move player up
+		}
+		if playerDown { //check if player down
+			playerDest.Y += playerSpeed //move player down
+		}
+		if playerLeft { //check if player left
+			playerDest.X -= playerSpeed //move player left
+		}
+		if playerRight { //check if player right
+			playerDest.X += playerSpeed //move player right
+		}
+		if frameCount % 8 == 0{ //check if frame count is divisible by 8
+			playerFrame++ //increment player frame
+		}
+		playerSrc.X = playerSrc.Width * float32(playerFrame) //update player source rectangle x
+
+	}
+
+	frameCount++ //increment frame count
+	if frameCount > 3 { 
+		frameCount = 0 //reset frame count
+	}
+
+	playerSrc.Y = playerSrc.Height * float32(playerDir) //update player source rectangle y
+
 	rl.UpdateMusicStream(music) //update music stream
 	if(musicPaused){ //check if music is paused
 		rl.PauseMusicStream(music) //pause music
@@ -57,6 +101,9 @@ func update(){ //update game
 	}
 
 	cam.Target = rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))) //update camera target
+
+	playerMoving = false //player not moving
+	playerUp, playerDown, playerLeft, playerRight = false, false, false, false //player up, down, left, right
 
 }
 
@@ -77,6 +124,8 @@ func init(){ //initialize game
 	rl.PlayMusicStream(music) //play music
 
 	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)), rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))), 0, 1) //initialize camera
+
+
 }
 
 func quit(){ //quit game
