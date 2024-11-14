9bc8f706c081790d6b00326b6cd08930f62953d1
anbguye <anthonybnguyen56@gmail.com>
2024-11-13 21:49:21 -0600

    added basic tile mapping

--- game.go
+++ game.go
@@ -1,10 +1,10 @@
 package main //created package name 
  
-import rl "github.com/gen2brain/raylib-go/raylib" //import raylib
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 const (
-	screenWidth = 1000 //screen width
-	screenHeight = 480 //screen height
+	screenWidth = 1000 
+	screenHeight = 480 
 )
 
 var (
@@ -13,14 +13,23 @@ var (
 
 	grassSprite rl.Texture2D //grass sprite
 	playerSprite rl.Texture2D //player sprite
-	playerSrc    rl.Rectangle  //player source rectangle
-	playerDest   rl.Rectangle  //player destination rectangle
-	playerMoving bool //player moving
+
+
+
+	playerSrc    rl.Rectangle
+	playerDest   rl.Rectangle 
+	playerMoving bool 
 	playerDir int //player direction
-	playerUp, playerDown, playerLeft, playerRight bool //player up, down, left, right
+	playerUp, playerDown, playerLeft, playerRight bool 
 	playerFrame int //player frame
 
 	frameCount int //frame count
+
+	tileDest rl.Rectangle 
+	tileSrc rl.Rectangle 
+	tileMap []int 
+	srcMap []string
+	mapWidth, mapHeight int
 
 	playerSpeed float32 = 3 //player speed
 
@@ -31,30 +40,43 @@ var (
 )
 
 func drawScene() { //draw scene
-	rl.DrawTexture(grassSprite, 100, 50, rl.White) //draw grass sprite
+
+	for i := 0; i < len(tileMap); i++ {
+		if tileMap[i] != 0 {
+			tileDest.X = tileDest.Width * float32(i % mapWidth)
+			tileDest.Y = tileDest.Height * float32(i / mapWidth)
+			tileSrc.X = tileSrc.Width * float32((tileMap[i]-1) % int(grassSprite.Width/int32(tileSrc.Width)))
+			tileSrc.Y = tileSrc.Height * float32((tileMap[i]-1) / int(grassSprite.Width/int32(tileSrc.Width)))
+
+			
+			rl.DrawTexturePro(grassSprite, tileSrc, tileDest, rl.NewVector2(tileDest.Width, tileDest.Height), 0 , rl.White) //draw grass sprite
+
+		}
+	}
+
 	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0 , rl.White) //draw player sprite
 }
 
 func input() { //get input
 	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp){ //check if W or up key is pressed
-		playerMoving = true //player moving
+		playerMoving = true 
 		playerDir = 1 //player direction
-		playerUp = true //player up
+		playerUp = true
 	}
 	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown){ //check if S or down key is pressed
-		playerMoving = true //player moving
+		playerMoving = true 
 		playerDir = 0 //player direction
-		playerDown = true //player down
+		playerDown = true
 	}
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight){ //check if D or right key is pressed
-		playerMoving = true //player moving
+		playerMoving = true 
 		playerDir = 3 //player direction
-		playerRight = true //player right
+		playerRight = true 
 	}
 	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft){ //check if A or left key is pressed
-		playerMoving = true //player moving
+		playerMoving = true 
 		playerDir = 2 //player direction
-		playerLeft = true //player left
+		playerLeft = true 
 	}
 	if rl.IsKeyPressed(rl.KeyQ){ //check if Q key is pressed
 		musicPaused = !musicPaused //toggle music paused
@@ -64,7 +86,7 @@ func input() { //get input
 func update(){ //update game
 	running = !rl.WindowShouldClose() //check if window should close
 
-	playerSrc.X = 0 //reset player source rectangle x
+	playerSrc.X = 0 
 
 	if playerMoving { //check if player moving
 		if playerUp { //check if player up
@@ -79,18 +101,25 @@ func update(){ //update game
 		if playerRight { //check if player right
 			playerDest.X += playerSpeed //move player right
 		}
-		if frameCount % 8 == 0{ //check if frame count is divisible by 8
-			playerFrame++ //increment player frame
+		if frameCount % 8 == 1 {  
+			playerFrame++  
 		}
-		playerSrc.X = playerSrc.Width * float32(playerFrame) //update player source rectangle x
-
+	} else if frameCount % 45 == 1 {
+		playerFrame++ // idle animation
 	}
+	
 
-	frameCount++ //increment frame count
-	if frameCount > 3 { 
+	frameCount++ 
+	if frameCount > 25 { 
 		frameCount = 0 //reset frame count
 	}
 
+	if !playerMoving && playerFrame > 1 {
+		playerFrame = 0
+	}
+
+
+	playerSrc.X = playerSrc.Width * float32(playerFrame) //update player source rectangle x
 	playerSrc.Y = playerSrc.Height * float32(playerDir) //update player source rectangle y
 
 	rl.UpdateMusicStream(music) //update music stream
@@ -102,8 +131,17 @@ func update(){ //update game
 
 	cam.Target = rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))) //update camera target
 
-	playerMoving = false //player not moving
-	playerUp, playerDown, playerLeft, playerRight = false, false, false, false //player up, down, left, right
+	playerMoving = false
+	playerUp, playerDown, playerLeft, playerRight = false, false, false, false 
+
+}
+
+func loadMap(){
+	mapWidth = 5
+	mapHeight = 5
+	for i := 0; i < (mapWidth * mapHeight); i++ {
+		tileMap = append(tileMap, 1)
+	}
 
 }
 
@@ -113,10 +151,14 @@ func init(){ //initialize game
 	rl.SetTargetFPS(60) //set target frames per second
 
 	grassSprite = rl.LoadTexture("sprites/tilesets/Grass_tiles_v2.png") //load grass sprite
+
+	tileDest = rl.NewRectangle(0, 0, 16, 16)
+	tileSrc = rl.NewRectangle(0, 0, 16, 16)
+
 	playerSprite = rl.LoadTexture("sprites/char/basic-char-spritesheet.png") //load player sprite
 
-	playerSrc = rl.NewRectangle(0, 0, 48, 48) //player source rectangle
-	playerDest = rl.NewRectangle(200, 200, 100, 100) //player destination rectangle
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100) 
 
 	rl.InitAudioDevice() //initialize audio device
 	music = rl.LoadMusicStream("audio/music/Avery's Farm.mp3") //load music
@@ -125,6 +167,7 @@ func init(){ //initialize game
 
 	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)), rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))), 0, 1) //initialize camera
 
+	loadMap()
 
 }
 
